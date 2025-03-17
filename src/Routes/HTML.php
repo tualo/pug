5f6dc682ad4b1fe4f5824c3ecadc453fed9dd3a0
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-03-17 10:32:17 +0100

    fixes

---
+++ src/Routes/HTML.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tualo\Office\PUG\Routes;
+
+use Exception;
+use Tualo\Office\Basic\TualoApplication;
+use Tualo\Office\Basic\Route;
+use Tualo\Office\Basic\IRoute;
+use Tualo\Office\PUG\PUG as PUG;
+
+class HTML implements IRoute
+{
+    public static function register()
+    {
+
+
+
+        Route::add('/pug/local/html/(?P<tablename>[\w\-\_]+)/(?P<template>[\w\-\_]+)/(?P<id>.+)', function ($matches) {
+
+            try {
+                TualoApplication::contenttype('text/html');
+                $table = \Tualo\Office\DS\DSTable::instance($matches['tablename']);
+                $table->f('__id', '=', $matches['id']);
+                $data = $table->read()->get();
+                $_REQUEST['data'] = $data;
+
+
+                $payload_data = json_decode(@file_get_contents('php://input'), true);
+                if (is_array($payload_data)) {
+                    $_REQUEST['data'] = [$payload_data];
+                }
+
+                $_REQUEST['id'] = $matches['id'];
+
+                $html = PUG::render($matches['template'], $_REQUEST);
+                TualoApplication::body($html);
+                Route::$finished = true;
+            } catch (Exception $e) {
+                TualoApplication::body($e->getMessage());
+            }
+        }, array('get', 'post'), true);
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -6,6 +6,7 @@ if (class_exists("Tualo\Office\CMS\Routes\Page")){ }
 */
 
 require_once "Routes/PDF.php";
+require_once "Routes/HTML.php";
 require_once "Routes/Pug2.php";
 require_once "Checks/Paths.php";
 require_once "ValueRenderer.php";
