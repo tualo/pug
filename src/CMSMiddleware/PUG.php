81364687a68083299d0f65b65a484cd6b0ed57a2
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-09-07 08:10:03 +0200

    add barcode

--- src/CMSMiddleware/PUG.php
+++ src/CMSMiddleware/PUG.php
@@ -5,7 +5,7 @@ namespace Tualo\Office\PUG\CMSMiddleware;
 use Tualo\Office\PUG\PUG2;
 
 use Tualo\Office\Basic\TualoApplication as App;
-
+use Tualo\Office\PUG\Barcode;
 
 class PUG
 {
@@ -20,8 +20,17 @@ class PUG
         };
     }
 
+
+    public static function barcode(): callable
+    {
+        return function ($type, $data): PUG2 {
+            return Barcode::get($type, $data);
+        };
+    }
+
     public static function run(&$request, &$result)
     {
         $result['pug'] = self::pug();
+        $result['barcode'] = self::barcode();
     }
 }
